5a45df115c98d469de1918ee23399772708e6da7
Artem <elagin.artenka@gmail.com>
2026-06-27 20:38:54 +0300

    Add DB models and register in admin

--- main/admin.py
+++ main/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import Course
+from .models import Profile, Lesson, Course, Submission
 
+admin.site.register(Profile)
+admin.site.register(Lesson)
 admin.site.register(Course)
+admin.site.register(Submission)
